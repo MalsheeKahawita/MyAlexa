e45b23f320607bb42d63054949f136a3a2ee2729
malshee <it20622114@my.sliit.lk>
2022-11-06 22:22:45 +0530

    Commit 2

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import speech_recognition as sr
+import pyttsx3
 
 listner = sr.Recognizer()
+engine = pyttsx3.init()
 
 try:
     with sr.Microphone() as source:
         print('listening....')
         voice = listner.listen(source)
         command = listner.recognize_google(voice)
-        print(command)
+        command = command.lower()
+        if 'alexa' in command:
+            print(command)
 except:
     pass
