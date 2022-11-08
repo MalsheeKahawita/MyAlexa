456457c32a65304eca527b7bbe10ae5ac286b2ca
malshee <it20622114@my.sliit.lk>
2022-11-08 16:11:33 +0530

    command text added

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ listner = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.getProperty('voices', voices[1])
-engine.say('I am your alexa')
-engine.say('What can I do for you')
-engine.runAndWait()
 
-try:
-    with sr.Microphone() as source:
-        print('listening....')
-        voice = listner.listen(source)
-        command = listner.recognize_google(voice)
-        command = command.lower()
-        if 'alexa' in command:
-            print(command)
-except:
-    pass
+def talk (text):
+    engine.say('text')
+    engine.runAndWait()
+
+def take_command:
+    try:
+        with sr.Microphone() as source:
+            print('listening....')
+            voice = listner.listen(source)
+            command = listner.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                talk(command)
+    except:
+        pass
+    return command
